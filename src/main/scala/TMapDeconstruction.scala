16352c5151fc8c03c84fc18c35e6e93eda2298ce
Martin Mauch <martin.mauch@gmail.com>
2016-03-15 20:42:10 +0100

    Add working tagging example

--- src/main/scala/TMapDeconstruction.scala
+++ src/main/scala/TMapDeconstruction.scala
@@ -1,21 +1,78 @@
+import Tagging._
+
 import scala.reflect.runtime.universe._
 
 object TMapDeconstruction extends App {
-  trait Show[T] {
-    def show(t: T): String
-  }
-  implicit val showInt = new Show[Int] {
-    def show(t: Int) = s"Got int $t"
-  }
-  implicit val showString = new Show[String] {
-    def show(t: String) = s"Got String $t"
+
+  sealed class =!=[A, B]
+
+  trait LowerPriorityImplicits {
+    /** do not call explicitly! */
+    implicit def equal[A]: =!=[A, A] = sys.error("should not be called")
   }
 
-  implicit def showAllFromTMap[T, V](implicit tShow: Show[T], vShow: Show[V],
-    ttt: TypeTag[T], ttv: TypeTag[V]) = new Show[TMap[T with V]] {
-    def show(m: TMap[T with V]): String = tShow.show(m.apply[T]) + ", " + vShow.show(m.apply[V])
+  object =!= extends LowerPriorityImplicits {
+    /** do not call explicitly! */
+    implicit def nequal[A, B]: =!=[A, B] = new =!=[A, B]
   }
 
-  val m = (TMap(1) ++ TMap("foo"))
-  println(implicitly[Show[TMap[Int with String]]].show(m))
+  sealed trait Kpi
+
+  class Impressions extends Kpi
+
+  class Clicks extends Kpi
+
+  class Ctr extends Kpi
+
+  class ConversionRate extends Kpi
+
+  class Conversions extends Kpi
+
+
+  trait Show[T, O] {
+    def show(t: T): O
+  }
+
+  implicit val showInt = new Show[Int, String] {
+    def show(t: Int) = s"int $t"
+  }
+  implicit val showString = new Show[String, String] {
+    def show(t: String) = s"String $t"
+  }
+  implicit val showImpressions = new Show[Double @@ Impressions, String @@ Impressions] {
+    def show(t: Double @@ Impressions) = s"Impressions $t".tag[Impressions]
+  }
+
+  implicit val showCtr = new Show[Double @@ Ctr, String @@ Ctr] {
+    def show(t: Double @@ Ctr) = s"Ctr $t".tag[Ctr]
+  }
+  implicit val showCr = new Show[Double @@ ConversionRate, String @@ ConversionRate] {
+    def show(t: Double @@ ConversionRate) = s"Cr $t".tag[ConversionRate]
+  }
+
+  implicit def showClicks[I, C](implicit showImpressions: Show[I @@ Impressions, String @@ Impressions],
+                                showCtr: Show[C @@ Ctr, String @@ Ctr],
+                                tti: TypeTag[I @@ Impressions],
+                                ttc: TypeTag[C @@ Ctr]
+                               ) = new Show[TMap[(I @@ Impressions) with (C @@ Ctr)], String @@ Clicks] {
+    def show(t: TMap[(I @@ Impressions) with (C @@ Ctr)]): String @@ Clicks =
+      s"""Clicks from ${showImpressions.show(t.apply[I @@ Impressions])}
+          |and ${showCtr.show(t.apply[C @@ Ctr])}
+       """.stripMargin.tag[Clicks]
+  }
+
+  implicit def showConversions(
+                                implicit
+                                showClicks: Show[TMap[(Double @@ Impressions) with (Double @@ Ctr)], String @@ Clicks],
+                                showCr: Show[Double @@ ConversionRate, String @@ ConversionRate]) = new Show[TMap[(Double @@ Impressions) with (Double @@ Ctr) with (Double @@ ConversionRate)], String @@ Conversions] {
+    override def show(t: TMap[(Double @@ Impressions) with (Double @@ Ctr) with (Double @@ ConversionRate)]): @@[String, Conversions] =
+      s"""Conversions from ${showClicks.show(TMap(t.apply[Double @@ Impressions]) ++ TMap(t.apply[Double @@ Ctr]))}
+          |and ${showCr.show(t.apply[Double @@ ConversionRate])}
+       """.stripMargin.tag[Conversions]
+  }
+
+  val p = TMap(0.5.tag[Impressions]) ++ TMap(0.3.tag[Ctr]) ++ TMap(0.05.tag[ConversionRate])
+  println(implicitly[Show[TMap[(Double @@ Impressions) with (Double @@ Ctr)], String @@ Clicks]].show(p))
+  println(implicitly[Show[TMap[(Double @@ Impressions) with (Double @@ Ctr) with (Double @@ ConversionRate)], String @@ Conversions]].show(p))
+
 }
---
+++ src/main/scala/Tagging.scala
@@ -0,0 +1,27 @@
+object Tagging {
+  trait Tagged[+V, +T]
+  type @@[+V, +T] = V with Tagged[V, T]
+
+  implicit class Taggable[V](val value: V) {
+    def tag[T] = value.asInstanceOf[V @@ T]
+
+    def tagAs[T <: V @@ _] = value.asInstanceOf[T]
+  }
+  implicit class Untaggable[V, T](val tagged: V with Tagged[V, T]) {
+    def untag: V = tagged
+  }
+
+  implicit def TaggedNumeric[V, T](implicit num: Numeric[V]) = new Numeric[V @@ T] {
+    override def plus(x: @@[V, T], y: @@[V, T]): @@[V, T] = num.plus(x.untag, y.untag).tag[T]
+    override def negate(x: @@[V, T]): @@[V, T] = num.negate(x.untag).tag[T]
+    override def times(x: @@[V, T], y: @@[V, T]): @@[V, T] = num.times(x.untag, y.untag).tag[T]
+    override def minus(x: @@[V, T], y: @@[V, T]): @@[V, T] = num.minus(x.untag, y.untag).tag[T]
+    override def compare(x: @@[V, T], y: @@[V, T]): Int = num.compare(x.untag, y.untag)
+    override def toDouble(x: @@[V, T]): Double = num.toDouble(x.untag)
+    override def toFloat(x: @@[V, T]): Float = num.toFloat(x.untag)
+    override def toInt(x: @@[V, T]): Int = num.toInt(x.untag)
+    override def toLong(x: @@[V, T]): Long = num.toLong(x.untag)
+    override def fromInt(x: Int): @@[V, T] = num.fromInt(x).tag[T]
+  }
+
+}
